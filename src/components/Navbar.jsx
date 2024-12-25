d2925a9db93ff6dcb9fca47b49a5dcf632538b5f
joimar755 <joimarjose19@gmail.com>
2024-12-24 23:53:03 -0500

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,17 @@ import React from "react"
 
 const Navbar = () => {
   return (
-    <nav className="bg-primary text-white py-5">
-        <div className="container flex justify-between items-center">
-            <h1 className="text-3xl text-red-500 flex justify-center items-center gap-2">My Website</h1>
-            <ul className="flex flex-row items-center gap-5 lg:gap-8 text-lg font-semibold">
-                <li><a className="rounded-[20px] bg-white p-5 text-black inline-block px-3 py-2  hover:text-yellow-400 transition-colors duration-100" href="/">Home</a></li>
-                <li><a className="rounded-[20px] bg-white p-5 text-black inline-block px-3 py-2 hover:text-yellow-400 transition-colors duration-100" href="/contact">Contact</a></li>
-                <li><a className="rounded-[20px] bg-white p-5 text-black inline-block px-3 py-2 hover:text-yellow-400 transition-colors duration-100" href="/about">About</a></li>
-            </ul>
-        </div>
-    </nav>
+    <>
+       <div className="flex justify-between items-center text-black py-6 px-8 md:px-32  drop-shadow-xl">
+        <h1 className="text-2xl font-bold">Eleva</h1>
+        <ul className="flex xl:flex md:flex items-center gap-12 font-semibold text-base">
+            <li><a className="box-shadow rounded-[25px] bg-white py-4  w-28 text-black text-center inline-block px-3 py-2 hover:text-yellow-400 transition-colors duration-100" href="/">Servicios</a></li>
+            <li><a className="box-shadow rounded-[25px] bg-white py-4  w-28 text-black text-center inline-block px-3 py-2 hover:text-yellow-400 transition-colors duration-100" href="/contact">Blog</a></li>
+            <li><a className="box-shadow rounded-[25px] bg-white py-4  w-28 text-black text-center inline-block px-3 py-2 hover:text-yellow-400 transition-colors duration-100" href="/about">Nosotros</a></li>
+            <li><a className="box-shadow rounded-[25px] bg-white py-4  w-28 text-black text-center inline-block px-3 py-2 hover:text-yellow-400 transition-colors duration-100" href="/blog">Contacto</a></li>
+        </ul>
+       </div>
+    </>
   )
 }
 
