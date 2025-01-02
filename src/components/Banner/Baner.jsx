bed4ccd68da4108c4a440ba6db4adfa0e23e3e3b
joimar755 <joimarjose19@gmail.com>
2025-01-02 16:02:30 -0500

    a

--- src/components/Banner/Baner.jsx
+++ src/components/Banner/Baner.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import img1 from '../../assets/img/home.jpg'
 
+
+
 const Baner = () => {
   return (
     <div className="max-w-[1640px] mx-auto p-4">
       <div className="max-h-[600px] relative">
         <div className="absolute w-full h-full flex flex-col justify-end items-center">
-            <h1 className="px-4 text-center text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-white">Somos <span className="text-blue-300">eleva!!</span></h1>
+            <h1 className="px-4 text-center text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-white">👋Somos <span className="text-blue-300">eleva!!</span></h1>
             <p class="px-4 text-center text-3xl sm:text-5xl md:text-6xl lg:text-4xl py-10 font-serif text-gray-100 ">Agencia desarrollo de productos</p>
 
         </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,9 @@ const Navbar = () => {
   
   return (
     <>
-       <div className="flex justify-between items-center text-black py-6 px-8 md:px-32  drop-shadow-xl">
+      <nav className="relative top-0 right-0 w-full z-50  sm:py-0">
+      <div className="relative">
+      <div className="flex justify-between items-center text-black py-6 px-8 md:px-32  drop-shadow-xl">
         <h1 className="text-5xl font-extrabold">Eleva</h1>
         <ul className="hidden  lg:flex  items-center gap-12 font-semibold text-base">
             <li><a className="box-shadow rounded-[25px] bg-white py-4  w-28 text-black text-center inline-block px-3 py-2 hover:text-yellow-400 transition-colors duration-100" href="/">Servicios</a></li>
@@ -19,14 +21,16 @@ const Navbar = () => {
         <div className="lg:hidden block text-5xl cursor-pointer" onClick={()=>setIsOpen(!isOpen)}>
            <CgMenuRightAlt />
         </div>
-        <div className={`absolute lg:hidden top-24 left-0 w-full h-full  flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform 
-          ${isOpen ? "opacity-100" : "opacity-0"}`}>
+        <div className={`absolute  lg:hidden top-24 left-0 w-full h-full  flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform 
+          ${isOpen ? "opacity-100 z-20" : "opacity-0 pointer-events-none z-[-1]"}`}>
             <li className="list-none"><a className="box-shadow rounded-[25px] bg-white py-4  w-96 text-black text-center inline-block px-3 py-2 hover:text-yellow-400 transition-colors duration-100" href="/">Servicios</a></li>
             <li className="list-none"><a className="box-shadow rounded-[25px] bg-white py-4  w-96 text-black text-center inline-block px-3 py-2 hover:text-yellow-400 transition-colors duration-100" href="/contact">Blog</a></li>
             <li className="list-none"><a className="box-shadow rounded-[25px] bg-white py-4  w-96 text-black text-center inline-block px-3 py-2 hover:text-yellow-400 transition-colors duration-100" href="/about">Nosotros</a></li>
             <li className="list-none"><a className="box-shadow rounded-[25px] bg-white py-4  w-96 text-black text-center inline-block px-3 py-2 hover:text-yellow-400 transition-colors duration-100" href="/blog">Contacto</a></li>
         </div>
        </div>
+      </div>
+      </nav>
     </>
   )
 }
